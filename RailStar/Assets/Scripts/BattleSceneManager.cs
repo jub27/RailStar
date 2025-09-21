6c7ca1d881e941a61986fddd77d138834c2cc53b
jub27 <55947009+jub27@users.noreply.github.com>
2025-09-21 18:58:34 +0900

    적 생성 구현

--- RailStar/Assets/Scripts/BattleSceneManager.cs
+++ RailStar/Assets/Scripts/BattleSceneManager.cs
@@ -1,10 +1,27 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleSceneManager : MonoBehaviour
 {
-    private GameObject[] enemies;
-    private GameObject[] playerCharacters;
-    
-    
+    [SerializeField] private Enemy enemyPrefab;
+    [SerializeField] private Transform[] enemySpawnTransform;
+    private HashSet<Enemy> enemySet;
+
+    private void Awake()
+    {
+        enemySet = new HashSet<Enemy>();
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < enemySpawnTransform.Length; i++)
+        {
+            var enemy = Instantiate(enemyPrefab, enemySpawnTransform[i]);
+            enemy.transform.localScale = Vector3.one;
+            enemy.transform.localPosition = Vector3.zero;
+            enemy.transform.localRotation = Quaternion.identity;
+            enemySet.Add(enemy);
+        }
+    }
 }
